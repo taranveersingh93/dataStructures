69a5cd364182d7cf520fca158df2a8976b3ce067
Taranveer Singh <taranveersingh93@gmail.com>
2024-08-02 01:53:48 -0400

    Add linkedlist variables and constructors

--- src/main/java/DataStructures/LinkedList.java
+++ src/main/java/DataStructures/LinkedList.java
@@ -5,5 +5,23 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Node;
 
 public class LinkedList {
+    private static final Logger log = LoggerFactory.getLogger(LinkedList.class);
+    private Node head;
+    private Node tail;
+    private int length;
 
+    public LinkedList(int value) {
+        //create a new node and head points to it
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+    public class Node {
+        public int value;
+        Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 }
