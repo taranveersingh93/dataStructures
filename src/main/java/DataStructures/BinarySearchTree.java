9e4ee3c08a6038f5b01948b4193e2a69a1a1c320
Taranveer Singh <taranveersingh93@gmail.com>
2024-08-06 05:52:00 -0400

    Add insert method

--- src/main/java/DataStructures/BinarySearchTree.java
+++ src/main/java/DataStructures/BinarySearchTree.java
@@ -15,4 +15,34 @@ public class BinarySearchTree {
         Node newNode = new Node(value);
         root = newNode;
     }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (this.root == null) {
+            this.root = newNode;
+            return true;
+        }
+        Node temp = this.root;
+        while (temp != null) {
+            if (temp.value == value) {
+                return false;
+            }
+            if (temp.value < value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                } else {
+                    temp = temp.left;
+                }
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                } else {
+                    temp = temp.right;
+                }
+            }
+        }
+        return false;
+    }
 }
