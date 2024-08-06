ff8b9da913da6f31d0d6b40e9a10a1129e074b3a
Taranveer Singh <taranveersingh93@gmail.com>
2024-08-06 07:54:28 -0400

    Add get method

--- src/main/java/DataStructures/HashTable.java
+++ src/main/java/DataStructures/HashTable.java
@@ -51,7 +51,17 @@ public class HashTable {
                 }
                 temp.next = newNode;
             }
+        }
 
-    }
-
+        public int get(String key) {
+            int index = hash(key);
+            Node temp = dataMap[index];
+            while (temp != null) {
+                if (temp.key.equals(key)) {
+                    return temp.value;
+                }
+                temp = temp.next;
+            }
+            return 0;
+        }
 }
